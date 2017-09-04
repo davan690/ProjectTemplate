cb396d35ef7c8220f6ebc7dfd6507fb57ae93fb0
Gionata Ghiggi <gionata.ghiggi@gmail.com>
2017-09-04 19:24:56 +0200

    Update how document functions.R

--- R/how document functions.R
+++ R/how document functions.R
@@ -1,6 +1,7 @@
 #' How to document functions.
 #'
 #' This is an example showing how to document functions to be included in the package.
+#' Use `Roxygen` `@import` or `@importFrom` in function definitions to import their package dependencies in the namespace.
 #'
 #' @export
 #' @name examplefunction
@@ -9,6 +10,7 @@
 #' @seealso \url{http://r-pkgs.had.co.nz/man.html}
 #' @examples
 #' examplefunction('Hello world!')
+
 
 examplefunction <- function(text){
   return(text)
