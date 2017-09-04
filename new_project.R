5a1f51c3c51c55dc9fab47d1a3b109343aa96494
ghiggi <gionata.ghiggi@gmail.com>
2017-09-04 19:48:48 +0200

    Update function 4 folders

--- new_project.R
+++ new_project.R
@@ -1,4 +1,3 @@
-
 #' Create new project scaffolding.
 #'#'
 #' @param name Name of the new project. A new folder will be created with that name.
@@ -17,8 +16,19 @@ new_project <- function(name, github = FALSE, private.repo = TRUE, travis = FALS
   use_data_raw(name)
   use_testthat(name)
   dir.create(file.path(name, "data"))
-  dir.create(file.path(name, "analyses"))
+  dir.create(file.path(name, "data\raw"))
+  dir.create(file.path(name, "data\clean"))
+  dir.create(file.path(name, "data\final"))
+  dir.create(file.path(name, "data\temp"))
+  dir.create(file.path(name, "docs"))
+  dir.create(file.path(name, "docs\man"))
+  dir.create(file.path(name, "docs\vignettes"))
+  dir.create(file.path(name, "figs"))
   dir.create(file.path(name, "manuscript"))
+  dir.create(file.path(name, "R"))
+  dir.create(file.path(name, "scripts"))
+  dir.create(file.path(name, "src"))
+  dir.create(file.path(name, "tests"))
   cat("\n^analyses$ \n^manuscript$ \n", sep = "",
       file = file.path(name, ".Rbuildignore"), append = TRUE)
 
