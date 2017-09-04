e817300503bccc94f66fde3018883db6ce345959
ghiggi <gionata.ghiggi@gmail.com>
2017-09-04 19:51:56 +0200

    Move function

--- R/new_project.R
+++ R/new_project.R
@@ -1,4 +1,3 @@
-
 #' Create new project scaffolding.
 #'#'
 #' @param name Name of the new project. A new folder will be created with that name.
@@ -16,10 +15,23 @@ new_project <- function(name, github = FALSE, private.repo = TRUE, travis = FALS
   try(use_readme_rmd(name), silent = TRUE)
   use_data_raw(name)
   use_testthat(name)
+  
   dir.create(file.path(name, "data"))
-  dir.create(file.path(name, "analyses"))
+  dir.create(file.path(name, "data\raw"))
+  dir.create(file.path(name, "data\clean"))
+  dir.create(file.path(name, "data\final"))
+  dir.create(file.path(name, "data\temp"))
+  dir.create(file.path(name, "docs"))
+  dir.create(file.path(name, "docs\man"))
+  dir.create(file.path(name, "docs\vignettes"))
+  dir.create(file.path(name, "figs"))
   dir.create(file.path(name, "manuscript"))
-  cat("\n^analyses$ \n^manuscript$ \n", sep = "",
+  dir.create(file.path(name, "R"))
+  dir.create(file.path(name, "scripts"))
+  dir.create(file.path(name, "src"))
+  dir.create(file.path(name, "tests"))
+  # Specify folders to not syncronize with git
+  # cat("\n^figs$ \n^manuscript$ \n", sep = "",
       file = file.path(name, ".Rbuildignore"), append = TRUE)
 
   if (github){
